03ad68a1e193981bf5ab6037fa865e054a605fd7
Jonathan <jonathantoledo11@gmail.com>
2021-11-06 21:57:37 -0700

    commit tree 6e015b44670c20e3b689175cd9794d3f13718ee4

---
+++ index.js
@@ -0,0 +1,31 @@
+let x = 2000;
+let y = 100;
+const divide = () => {
+    console.log(divide);
+    console.log(x = 2000);
+    console.log(y = 100);
+    return x / y;
+} 
+
+divide();
+
+let parameter1 = 5
+let parameter2 = 2
+const add = () => {
+    console.log(add);
+    console.log(parameter1 = 5);
+    console.log(parameter2 = 2);
+    return parameter1 + parameter2;
+} 
+add();
+
+let a = 2;
+let b = 2;
+
+const square =() => {
+    console.log(square);
+    console.log(a = 2);
+    console.log(b = 2);
+    return a * b;
+}
+square();
